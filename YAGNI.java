45793c3429b4a50d19ac43a19e85a54026b058e3
Zhansaya Zhumagazina <165804047+zhansaya7@users.noreply.github.com>
2026-02-01 21:12:03 +0500

    Create YAGNI.java

---
+++ YAGNI.java
@@ -0,0 +1,18 @@
+public class YAGNI {
+
+    class User {
+        String name;
+        String email;
+    }
+
+    class FileReader {
+        String readFile(String path) {
+            return "file content";
+        }
+    }
+
+    class ReportGenerator {
+        void generatePdf() {
+        }
+    }
+}
